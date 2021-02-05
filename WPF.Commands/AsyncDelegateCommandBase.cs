c41295d609e298b920b033b3718c4d64a33e0600
Максим <singsing243@gmail.com>
2021-02-05 18:07:27 +0300

    Добавьте файлы проекта.

---
+++ WPF.Commands/AsyncDelegateCommandBase.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WPF.Commands;
+
+namespace WPF.Commands
+{
+    /// <summary>
+    /// ICommand implementation for WPF bindings.
+    /// </summary>
+    public abstract class AsyncDelegateCommandBase : DelegateCommandBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AsyncDelegateCommandBase"/> class.
+        /// </summary>
+        /// <param name="freezeWhenBusy">UI not blocked when function executed, so user can trigger function multiple times at once. This will prevent it: during execution CanExecute would return false.</param>
+        /// <param name="executeOnce">Execute function only once, after that CanExecute would return false regardless.</param>
+        /// <param name="refreshAutomatically">A value indicating whether CanExecute should be refreshed automatically or manually.</param>
+        /// <param name="forceExecutionOnSeparateThread">A value indicating whether Execute must be forced to run on a non-UI thread.</param>
+        /// <param name="exceptionHandler">An exception handler function.</param>
+        protected AsyncDelegateCommandBase(bool freezeWhenBusy,
+                                           bool executeOnce,
+                                           bool refreshAutomatically,
+                                           Func<Exception, bool>? exceptionHandler = null)
+            : base(executeOnce, refreshAutomatically, exceptionHandler)
+        {
+            FreezeWhenBusy = freezeWhenBusy;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether command would return false when called CanExecute.
+        /// </summary>
+        protected bool FreezeWhenBusy { get; }
+
+        private bool IsBusy { get; set; }
+
+        /// <summary>
+        /// Implementation of <see cref="DelegateCommandBase" /> CanExecuteInternal.
+        /// </summary>
+        /// <param name="parameter">Parameter, provided in CommandParameter attribute. May be ignored.</param>
+        /// <returns>If this command can be executed.</returns>
+        protected sealed override bool CanExecuteInternal(object? parameter)
+        {
+            if (FreezeWhenBusy && IsBusy)
+            {
+                return false;
+            }
+
+            return CanExecuteAsyncInternal(parameter);
+        }
+
+        /// <summary>
+        /// Async means internal implementation for Async* commands.
+        /// </summary>
+        /// <param name="parameter">Parameter, provided in CommandParameter attribute. May be ignored.</param>
+        /// <returns>Returns value which indicates whether command can be executed.</returns>
+        protected abstract bool CanExecuteAsyncInternal(object? parameter);
+
+        /// <summary>
+        /// Implementation of <see cref="DelegateCommandBase" /> ExecuteInternal.
+        /// </summary>
+        /// <param name="parameter">Parameter, provided in CommandParameter attribute. May be ignored.</param>
+        protected sealed override async void ExecuteInternal(object? parameter)
+        {
+            IsBusy = true;
+            if (FreezeWhenBusy)
+            {
+                RaiseCanExecuteChanged();
+            }
+
+            try
+            {
+                await ExecuteAsyncInternal(parameter);
+            }
+            catch (Exception exception)
+            {
+                bool isHandeled = HandleException(exception);
+                if (!isHandeled)
+                {
+                    throw;
+                }
+            }
+            finally
+            {
+                IsBusy = false;
+                RaiseCanExecuteChanged();
+            }
+        }
+
+        /// <summary>
+        /// Async means internal implementation for Async* commands.
+        /// </summary>
+        /// <param name="parameter">Parameter, provided in CommandParameter attribute. May be ignored.</param>
+        /// <returns>Awaitable task.</returns>
+        protected abstract Task ExecuteAsyncInternal(object? parameter);
+    }
+}
---
+++ WPF.Demo/MainWindowViewModel.cs
@@ -0,0 +1,154 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using WPF.Commands;
+
+namespace WPF.Demo
+{
+    [AddINotifyPropertyChangedInterface]
+    public class MainWindowViewModel
+    {
+        public int Count { get; set; }
+        private void IncrementCount() => Count++;
+        private async Task AsyncIncrementCount() => Count++;
+
+        public MainWindowViewModel()
+        {
+            DelegateCommand.GlobalExceptionHandler += exception =>
+            {
+                MessageBox.Show($"Exception handled globally: {exception.Message}");
+                return true;
+            };
+
+            DelegateSetAndWaitCommand = new DelegateCommand(DelegateSetAndWait);
+            DelegateWaitAndSetCommand = new DelegateCommand(DelegateWaitAndSet);
+            DelegateExecuteOnceCommand = new DelegateCommand(IncrementCount, executeOnce: true);
+            DelegateCannotExecuteCommand = new DelegateCommand(null, canExecute: () => false);
+            DelegateCanExecuteManuallySetAutomaticallyUpdatedCommand = new DelegateCommand(IncrementCount, canExecute: () => CanExecuteFirst);
+            DelegateCanExecuteManuallySetManuallyUpdatedCommand = new DelegateCommand(IncrementCount, canExecute: () => CanExecuteSecond, refreshAutomatically: false);
+            UpdateCanExecuteCommand = new DelegateCommand(UpdateCanExecute);
+            DelegateGenericCommand = new DelegateCommand<string>(DelegateGeneric);
+
+            DelegateExceptionHandledCommand = new DelegateCommand(DelegateExceptionHandled, exceptionHandler: exception =>
+            {
+                MessageBox.Show($"Exception handled locally: {exception.Message}");
+                return true;
+            });
+            DelegateExceptionCommand = new DelegateCommand(DelegateExceptionHandled);
+
+            AsyncDelegateSetAndWaitCommand = new AsyncDelegateCommand(AsyncDelegateSetAndWait);
+            AsyncDelegateSetAndWaitDisabledWhenBusyCommand = new AsyncDelegateCommand(AsyncDelegateSetAndWait, freezeWhenBusy: true);
+            AsyncDelegateSetAndWaitDisabledWhenBusyManualCommand = new AsyncDelegateCommand(AsyncDelegateSetAndWait, freezeWhenBusy: true, refreshAutomatically: false);
+            AsyncDelegateWaitAndSetCommand = new AsyncDelegateCommand(AsyncDelegateWaitAndSet);
+            AsyncDelegateExecuteOnceCommand = new AsyncDelegateCommand(AsyncIncrementCount, executeOnce: true);
+            AsyncDelegateCannotExecuteCommand = new AsyncDelegateCommand(null, canExecute: () => false);
+            AsyncDelegateCanExecuteManuallySetAutomaticallyUpdatedCommand = new AsyncDelegateCommand(AsyncIncrementCount, canExecute: () => AsyncCanExecuteFirst);
+            AsyncDelegateCanExecuteManuallySetManuallyUpdatedCommand = new AsyncDelegateCommand(AsyncIncrementCount, canExecute: () => AsyncCanExecuteSecond, refreshAutomatically: false);
+            AsyncUpdateCanExecuteCommand = new AsyncDelegateCommand(AsyncUpdateCanExecute);
+            AsyncDelegateGenericCommand = new AsyncDelegateCommand<string>(AsyncDelegateGeneric);
+
+            AsyncDelegateExceptionHandledCommand = new AsyncDelegateCommand(AsyncDelegateExceptionHandled, exceptionHandler: exception =>
+            {
+                MessageBox.Show($"Exception handled locally: {exception.Message}");
+                return true;
+            });
+            AsyncDelegateExceptionCommand = new AsyncDelegateCommand(AsyncDelegateExceptionHandled);
+        }
+
+        public DelegateCommand DelegateSetAndWaitCommand { get; }
+        private void DelegateSetAndWait()
+        {
+            IncrementCount();
+            Thread.Sleep(2000);
+        }
+
+        public DelegateCommand DelegateWaitAndSetCommand { get; }
+        private void DelegateWaitAndSet()
+        {
+            Thread.Sleep(2000);
+            IncrementCount();
+        }
+
+        public DelegateCommand DelegateExecuteOnceCommand { get; }
+        public DelegateCommand DelegateCannotExecuteCommand { get; }
+
+        public bool CanExecuteFirst { get; set; }
+        public DelegateCommand DelegateCanExecuteManuallySetAutomaticallyUpdatedCommand { get; }
+
+        public bool CanExecuteSecond { get; set; }
+        // Manually update CanExecute for a command
+        private void UpdateCanExecute()
+        {
+            DelegateCanExecuteManuallySetManuallyUpdatedCommand.RaiseCanExecuteChanged();
+        }
+
+        public DelegateCommand DelegateCanExecuteManuallySetManuallyUpdatedCommand { get; }
+        public DelegateCommand UpdateCanExecuteCommand { get; }
+
+        public DelegateCommand<string> DelegateGenericCommand { get; }
+        private void DelegateGeneric(string value)
+        {
+            Count += int.Parse(value);
+        }
+        
+        public AsyncDelegateCommand AsyncDelegateSetAndWaitDisabledWhenBusyCommand { get; }
+        public AsyncDelegateCommand AsyncDelegateSetAndWaitDisabledWhenBusyManualCommand { get; }
+        
+        public AsyncDelegateCommand AsyncDelegateSetAndWaitCommand { get; }
+        private async Task AsyncDelegateSetAndWait()
+        {
+            await AsyncIncrementCount();
+            await Task.Delay(2000);
+        }
+
+        public AsyncDelegateCommand AsyncDelegateWaitAndSetCommand { get; }
+        private async Task AsyncDelegateWaitAndSet()
+        {
+            await Task.Delay(2000);
+            await AsyncIncrementCount();
+        }
+
+        public AsyncDelegateCommand AsyncDelegateExecuteOnceCommand { get; }
+        public AsyncDelegateCommand AsyncDelegateCannotExecuteCommand { get; }
+
+        public bool AsyncCanExecuteFirst { get; set; }
+        public AsyncDelegateCommand AsyncDelegateCanExecuteManuallySetAutomaticallyUpdatedCommand { get; }
+
+        public bool AsyncCanExecuteSecond { get; set; }
+        // Manually update CanExecute for a command
+        private async Task AsyncUpdateCanExecute()
+        {
+            // By default, command executed on separate thread, so we need a dispatcher to address it
+            AsyncDelegateCanExecuteManuallySetManuallyUpdatedCommand.RaiseCanExecuteChanged();
+        }
+
+        public AsyncDelegateCommand AsyncDelegateCanExecuteManuallySetManuallyUpdatedCommand { get; }
+        public AsyncDelegateCommand AsyncUpdateCanExecuteCommand { get; }
+
+        public AsyncDelegateCommand<string> AsyncDelegateGenericCommand { get; }
+        private async Task AsyncDelegateGeneric(string value)
+        {
+            Count += int.Parse(value);
+        }
+
+        public DelegateCommand DelegateExceptionCommand { get; }
+        public DelegateCommand DelegateExceptionHandledCommand { get; }
+        private void DelegateExceptionHandled()
+        {
+            throw new Exception("Some exception");
+        }
+
+        public AsyncDelegateCommand AsyncDelegateExceptionCommand { get; }
+        public AsyncDelegateCommand AsyncDelegateExceptionHandledCommand { get; }
+        private async Task AsyncDelegateExceptionHandled()
+        {
+            throw new Exception("Some exception");
+        }
+    }
+}
